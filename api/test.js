d235759a946afb5667d34b82e0f6ca13b8bc7c09
NotepadOnline <dev@notepadonline.com>
2025-12-10 02:41:33 +0700

    Add simple test endpoint

---
+++ api/test.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    message: 'Test endpoint working!',
+    timestamp: new Date().toISOString(),
+  });
+}
+
